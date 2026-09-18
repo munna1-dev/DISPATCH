97503a8f4e912244caca5f7c479df72e5677d37d
munna1-dev <YOUR_GITHUB_EMAIL>
2026-09-18 09:54:58 -0400

    Fix admin HTML escaping helper

--- public/js/admin.js
+++ public/js/admin.js
@@ -7273,6 +7273,25 @@ function handleAdminSessionExpired() {
    JAVASCRIPT STRING SAFETY
 ========================================================= */
 
+function escapeHtml(
+  value
+) {
+  if (
+    value === null ||
+    value === undefined
+  ) {
+    return '';
+  }
+
+  return String(value)
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#39;');
+}
+
+
 function escapeJsString(
   value
 ) {
